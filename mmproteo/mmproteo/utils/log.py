eac99de45817d981bdf9714a697f5bcc26fb3f4d
Mirko Krause <mirko.krause@student.hpi.de>
2021-01-26 17:45:30 +0100

    type annotate logging methods

--- mmproteo/mmproteo/utils/log.py
+++ mmproteo/mmproteo/utils/log.py
@@ -1,26 +1,28 @@
 import os
 import sys
 import logging
+from typing import TextIO, Optional
+
 from mmproteo.utils.utils import ensure_dir_exists
 
-LOG_FORMAT = '%(asctime)s - %(name)s: %(message)s'
-DEFAULT_LOG_SUFFIX = '.log'
+LOG_FORMAT: str = '%(asctime)s - %(name)s: %(message)s'
+DEFAULT_LOG_SUFFIX: str = '.log'
 
 
 class Logger:
     def __init__(self, logger):
         self.logger = logger
 
-    def info(self, msg: str = ""):
+    def info(self, msg: str = "") -> None:
         self.logger.info(msg)
 
-    def debug(self, msg: str = ""):
+    def debug(self, msg: str = "") -> None:
         self.logger.debug("DEBUG: " + msg)
 
-    def error(self, msg: str = ""):
+    def error(self, msg: str = "") -> None:
         self.info("ERROR: " + msg)
 
-    def warning(self, msg: str = ""):
+    def warning(self, msg: str = "") -> None:
         self.info("WARNING: " + msg)
 
 
@@ -30,25 +32,25 @@ class DummyLogger(Logger):
         if send_welcome:
             self.info("Printing to Stdout")
 
-    def info(self, msg: str = ""):
+    def info(self, msg: str = "") -> None:
         print("INFO: " + msg)
 
-    def debug(self, msg: str = ""):
+    def debug(self, msg: str = "") -> None:
         print("DEBUG: " + msg)
 
-    def error(self, msg: str = ""):
+    def error(self, msg: str = "") -> None:
         print("ERROR: " + msg)
 
-    def warning(self, msg: str = ""):
+    def warning(self, msg: str = "") -> None:
         print("WARNING: " + msg)
 
 
 def create_logger(name: str,
-                  level=None,
-                  verbose: bool = None,
-                  filename: str = None,
+                  level: Optional[int] = None,
+                  verbose: Optional[bool] = None,
+                  filename: Optional[str] = None,
                   log_dir: str = ".",
-                  log_to_std=sys.stderr):
+                  log_to_std: Optional[TextIO] = sys.stderr) -> Logger:
     if verbose is not None:
         assert level is None, "level and verbose are exclusive parameters"
         if verbose:
@@ -90,11 +92,12 @@ def create_logger(name: str,
 
         return Logger(logger)
     except Exception as e:
-        print("Error: Failed to create logger", str(name))
-        print(e)
-        print("Returned print-only dummy logger")
         logger = DummyLogger()
+        logger.error("Failed to create logger " + str(name))
+        logger.debug(str(e))
+        logger.info("Returned print-only dummy logger")
+
         return logger
 
 
-DUMMY_LOGGER = DummyLogger(send_welcome=False)
+DUMMY_LOGGER: DummyLogger = DummyLogger(send_welcome=False)
